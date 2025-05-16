c7265ef924612df44d56a8a7023380a4244a574e
Ponmuthu <ponmuthusenthilkumar@gmail.com>
2025-05-16 09:23:21 +0530

    Initial commit - Enterprise CRM System

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -165,9 +165,9 @@ function formatDate(dateString) {
 }
 
 function formatCurrency(amount) {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
         style: 'currency',
-        currency: 'USD',
+        currency: 'INR',
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
     }).format(amount);
